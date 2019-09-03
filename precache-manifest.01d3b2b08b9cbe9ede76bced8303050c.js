a7202467bb3ee4d2c6e8d0b40315ea70f79d7c1e
Henry Maher <henry.maher99@gmail.com>
2019-09-03 16:40:07 +0100

    Updates

--- precache-manifest.01d3b2b08b9cbe9ede76bced8303050c.js
+++ precache-manifest.01d3b2b08b9cbe9ede76bced8303050c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ed033429c425e1381b18842074eb931",
+    "revision": "12d06cd2aa3bed60de782def183d904b",
     "url": "/personal-website/index.html"
   },
   {
-    "revision": "17840b029cb01f8df78d",
-    "url": "/personal-website/static/css/main.90142dcf.chunk.css"
+    "revision": "e23e803f370eee515d01",
+    "url": "/personal-website/static/css/main.2fbccf4b.chunk.css"
   },
   {
-    "revision": "3a2ed7931efb18a9da80",
-    "url": "/personal-website/static/js/2.56b25b7f.chunk.js"
+    "revision": "1f9768fe694c6ef54959",
+    "url": "/personal-website/static/js/2.51544c3e.chunk.js"
   },
   {
-    "revision": "17840b029cb01f8df78d",
-    "url": "/personal-website/static/js/main.b4304bf6.chunk.js"
+    "revision": "e23e803f370eee515d01",
+    "url": "/personal-website/static/js/main.dc23810b.chunk.js"
   },
   {
     "revision": "2036ea3d25188b8a7914",
@@ -22,6 +22,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "130189fb0fa9e7648022fdc6f812fbdd",
     "url": "/personal-website/static/media/circle.130189fb.png"
+  },
+  {
+    "revision": "78fa82c599dd9b2660162b94e44ed30d",
+    "url": "/personal-website/static/media/node.78fa82c5.png"
   },
   {
     "revision": "0aca7974558e1d219e02559034ae6c65",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.b11dc1c89092690a10f30406b04d6cb7.js"
+  "/personal-website/precache-manifest.01d3b2b08b9cbe9ede76bced8303050c.js"
 );
 
 self.addEventListener('message', (event) => {
